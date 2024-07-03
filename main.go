4abbb9f44221f7b612dd772caa6d7b5474d1702e
dinudk02 <dineshkumarwork02@gmail.com>
2024-07-03 13:11:42 +0530

    working

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
-// package main
+package main
 
-// import "github.com/gin-gonic/gin"
+import (
+	"github.com/dinudk02/golang/initializers"
+	"github.com/gin-gonic/gin"
+)
 
-// func main() {
-// 	r := gin.Default()
-// 	r.GET("/ping", func(c *gin.Context) {
-// 		c.JSON(200, gin.H{
-// 			"message": "pong",
-// 		})
-// 	})
-// 	r.Run()
-// }
+func init() {
+	initializers.LoadEnvVar()
+
+}
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong1234",
+		})
+	})
+	r.Run()
+}
